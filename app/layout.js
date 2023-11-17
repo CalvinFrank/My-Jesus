6217d3d6824ce27968def7f4eb8180a7190a101f
Calvin Frank <CalvinAndersonFrank@gmail.com>
2023-11-16 20:09:32 -0500

    v. 0.2.0 added title subtitle unfinished description

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>My Jesus</title>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -3,6 +3,10 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-      <div></div>
+      <div>
+        <h1 className={styles.title}>My Jesus</h1>
+        <p className={styles.subtitle}>This app is made by Calvin Frank for the Lord.</p>
+        <p>description <br />learn more on our blog.</p>
+      </div>
   )
 }
