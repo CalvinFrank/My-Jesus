30afd76e8a24118106612ab43695fdbd497dd8ec
Calvin Frank <CalvinAndersonFrank@gmail.com>
2023-11-17 16:29:33 -0500

    v. 0.2.0.1 still working

--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ import styles from './page.module.css'
 export default function Home() {
   return (
       <div>
+        <header className={styles.header}>
+          <p>hello</p>
+        </header>
         <h1 className={styles.title}>My Jesus</h1>
         <p className={styles.subtitle}>This app is made by Calvin Frank for the Lord.</p>
         <p>description <br />learn more on our blog.</p>
