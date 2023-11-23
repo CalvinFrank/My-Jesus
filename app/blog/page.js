456952dd79c205554ad0d442f445cfe6087a71f5
Calvin Frank <CalvinAndersonFrank@gmail.com>
2023-11-23 08:00:24 -0500

    v. 1.0.1 added My Jesus song

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,9 +4,10 @@ export default function Blog() {
   return (
     <div>
       <h1>About This Webesite</h1>
-      <p>This app is made so that the kingdom of god can be expanded.</p>
+      <p>This app is made so that the kingdom of god can be expanded.   This app was inspired by the song My Jesus:</p>
+      <a href="https://www.youtube.com/watch?v=SouXPjiMZD0">https://www.youtube.com/watch?v=SouXPjiMZD0</a>
       <h1>About The Creator</h1>
-      <p>Calvin is a 9 year old who codes and loves God.  He likes other things like football, friends, etc.</p>
+      <p>Calvin is a 9 year old who codes and loves God.  He also likes other things like football, friends, etc.</p>
     </div>
   )
 }
